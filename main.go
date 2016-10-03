6eda8f0c89188e304eb822f81d378fb996c62b16
hakobe <hakobe@gmail.com>
2016-10-03 23:20:48 +0900

    直接requestを保存しない

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"sort"
+	"strconv"
 	"sync"
+	"time"
 
 	"github.com/Songmu/strrand"
 	"github.com/dimfeld/httptreemux"
@@ -28,46 +33,116 @@ func _templates() map[string]*template.Template {
 
 type gomibakoKey string
 
-type gomibakoAddingReq struct {
-	key     string
-	request *http.Request
+type gomibakoRequest struct {
+	timestamp     time.Time
+	method        string
+	url           *url.URL
+	headers       http.Header
+	body          []byte
+	contentLength int64
+}
+
+type gomibako struct {
+	key      gomibakoKey
+	requests []*gomibakoRequest
 }
 
 type gomibakoRepository struct {
-	gomibakos map[gomibakoKey][]*http.Request
+	gomibakos map[gomibakoKey]*gomibako
 	mutex     sync.RWMutex
 }
 
 func newGomibakoRepository() *gomibakoRepository {
 	gr := gomibakoRepository{
-		gomibakos: make(map[gomibakoKey][]*http.Request),
+		gomibakos: make(map[gomibakoKey]*gomibako),
 		mutex:     sync.RWMutex{},
 	}
 	return &gr
 }
 
-func (gr *gomibakoRepository) Add(key gomibakoKey, req *http.Request) {
+func (gr *gomibakoRepository) AddGomibako() (*gomibako, error) {
 	gr.mutex.Lock()
 	defer gr.mutex.Unlock()
 
-	reqs, ok := gr.gomibakos[key]
-	if ok {
-		gr.gomibakos[key] = append(reqs, req)
-	} else {
-		gr.gomibakos[key] = []*http.Request{req}
+	str, err := strrand.RandomString("[a-z0-9]{10}")
+	if err != nil {
+		return nil, err
 	}
+	newKey := gomibakoKey(str)
+
+	gr.gomibakos[newKey] = &gomibako{
+		key:      newKey,
+		requests: make([]*gomibakoRequest, 0),
+	}
+
+	return gr.gomibakos[newKey], nil
 }
 
-func (gr *gomibakoRepository) Get(key gomibakoKey) []*http.Request {
+func (gr *gomibakoRepository) AddRequest(key gomibakoKey, greq *gomibakoRequest) error {
+	gr.mutex.Lock()
+	defer gr.mutex.Unlock()
+
+	g, ok := gr.gomibakos[key]
+	if !ok {
+		return errors.New("no gomibako found")
+	}
+	g.requests = append(g.requests, greq)
+	return nil
+}
+
+func (gr *gomibakoRepository) Get(key gomibakoKey) (*gomibako, error) {
 	gr.mutex.RLock()
 	defer gr.mutex.RUnlock()
 
-	reqs, ok := gr.gomibakos[key]
-	if ok {
-		return reqs
+	g, ok := gr.gomibakos[key]
+	if !ok {
+		return nil, errors.New("no gomibako found")
 	}
+	return g, nil
+}
 
-	return []*http.Request{}
+type ViewableHeaderPair struct {
+	Key   string
+	Value string
+}
+
+type ViewableHeaders []*ViewableHeaderPair
+
+func (hs ViewableHeaders) Len() int      { return len(hs) }
+func (hs ViewableHeaders) Swap(i, j int) { hs[i], hs[j] = hs[j], hs[i] }
+func (hs ViewableHeaders) Less(i, j int) bool {
+	if hs[i].Key == hs[j].Key {
+		return hs[i].Value < hs[j].Value
+	} else {
+		return hs[i].Key < hs[j].Key
+	}
+}
+
+type ViewableGomibakoRequest struct {
+	Timestamp     string
+	Method        string
+	URL           string
+	Headers       ViewableHeaders
+	Body          string
+	ContentLength string
+}
+
+func newViewableGomibakoRequest(greq *gomibakoRequest) *ViewableGomibakoRequest {
+	var viewableHeaders ViewableHeaders
+	for k, vs := range greq.headers {
+		for _, v := range vs {
+			viewableHeaders = append(viewableHeaders, &ViewableHeaderPair{k, v})
+		}
+	}
+	sort.Sort(viewableHeaders)
+	return &ViewableGomibakoRequest{
+		Timestamp:     greq.timestamp.String(),
+		Method:        greq.method,
+		URL:           greq.url.String(),
+		Headers:       viewableHeaders,
+		Body:          string(greq.body),
+		ContentLength: strconv.FormatInt(greq.contentLength, 10),
+	}
 }
 
 func main() {
@@ -87,43 +162,65 @@ func main() {
 		}
 	})
 	group.POST("/g/-/new", func(w http.ResponseWriter, r *http.Request) {
-		str, err := strrand.RandomString("[a-z0-9]{10}")
+		g, err := gr.AddGomibako()
 		if err != nil {
-			http.Error(w, "key generation error: "+err.Error(), http.StatusInternalServerError)
+			http.Error(w, "gomibako generation error: "+err.Error(), http.StatusInternalServerError)
 		}
-		http.Redirect(w, r, "/g/"+str+"/inspect", 302)
+		http.Redirect(w, r, "/g/"+string(g.key)+"/inspect", 302)
 	})
 	group.GET("/g/:gomibakokey/inspect", func(w http.ResponseWriter, r *http.Request) {
 		params := httptreemux.ContextParams(r.Context())
 		gomibakoKey := gomibakoKey(params["gomibakokey"])
-		reqs := gr.Get(gomibakoKey)
+		g, err := gr.Get(gomibakoKey)
+		if err != nil {
+			http.Error(w, "no gomibako found", http.StatusNotFound)
+			return
+		}
+
+		requests := make([]*ViewableGomibakoRequest, len(g.requests))
+		for i, r := range g.requests {
+			requests[(len(g.requests)-1)-i] = newViewableGomibakoRequest(r)
+		}
 
 		type Inventry struct {
 			Title    string
-			Requests []string
+			Requests []*ViewableGomibakoRequest
 		}
 
-		inv := Inventry{Title: "gomibako: " + string(gomibakoKey), Requests: make([]string, len(reqs))}
-		for i, req := range reqs {
-			inv.Requests[(len(reqs)-1)-i] = fmt.Sprintf("%v", req)
-		}
-		err := templates["inspect"].Execute(w, inv)
+		inv := Inventry{Title: "gomibako: " + string(gomibakoKey), Requests: requests}
+		err = templates["inspect"].Execute(w, inv)
 		if err != nil {
 			http.Error(w, "template error: "+err.Error(), http.StatusInternalServerError)
+			return
 		}
 	})
-	group.GET("/g/:gomibakokey", func(w http.ResponseWriter, r *http.Request) {
+	recordReq := func(w http.ResponseWriter, r *http.Request) {
 		params := httptreemux.ContextParams(r.Context())
 		gomibakoKey := gomibakoKey(params["gomibakokey"])
 
-		gr.Add(gomibakoKey, r)
+		reader := http.MaxBytesReader(w, r.Body, 3*1000*1000)
+		defer reader.Close()
+		bodyBytes, err := ioutil.ReadAll(reader)
+		if err != nil {
+			http.Error(w, "failed to load body", http.StatusBadRequest)
+			return
+		}
+
+		greq := &gomibakoRequest{
+			timestamp: time.Now(),
+			method:    r.Method,
+			url:       r.URL,
+			headers:   r.Header,
+			body:      bodyBytes,
+		}
+		gr.AddRequest(gomibakoKey, greq)
 
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
 		io.WriteString(w, "ok\n")
-	})
-	group.POST("/g/:gomibakokey", func(w http.ResponseWriter, r *http.Request) {
-	})
+	}
+	group.GET("/g/:gomibakokey", recordReq)
+	group.POST("/g/:gomibakokey", recordReq)
 
 	n := negroni.Classic()
 	n.UseHandler(router)
